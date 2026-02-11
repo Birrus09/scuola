80ba6d395ec01a9c0be7ce8e00ea42344e435733
Birrus09 <simone.verganj@gmail.com>
2026-02-11 23:04:52 +0100

    Update generatore.cpp

--- generatore.cpp
+++ generatore.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <string>
+#include <ctime>
 
 using namespace std;
+
 int main() {
-    ofstream out("output.txt");
-    int N = 256;
-    float arr[N];
-    for (int i = 0; i < N; i++){
-        arr[i] = rand() / (float)RAND_MAX;
-        out << arr[i] << endl;
+    int Maxn, Minn, N, range;
+    clock_t inizio = clock()
+    string nomefile;
+
+    cout << " inserisci minimo, massimo e numeri da generare (max 8192): ";
+    cin >> Minn;
+    cin >> Maxn;
+    cin >> N;
+    range = Maxn-Minn;
+    cout << "inserisci il nome del file su cui scrivere: ";
+    cin >> nomefile;
+
+
+    ofstream outfile(nomefile + ".txt"); 
+    for(int i = 0; i <N; i++){
+        outfile << rand() % range + Minn << endl;
     }
-    out.close();
-    return 0;
+    cout << "impiegato " << (inizio-clock())/(CLOCKS_PER_SEC*1000.0)
+    outfile.close();
 }
-
-
