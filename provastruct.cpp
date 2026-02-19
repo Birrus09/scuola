f559c72edc72a8809c32eee156c5ad2478fb5c0d
Birrus09 <simone.verganj@gmail.com>
2026-02-19 08:24:35 +0100

    hhh
    Implement a struct and demonstrate its usage in main.

---
+++ provastruct.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct pippo {
+	int pluto;
+	string paperino;
+};
+
+
+int main() {
+	pippo pippo1;
+	pippo1.pluto = 5;
+	cout << pippo1.pluto;
+	return 0;
+}
